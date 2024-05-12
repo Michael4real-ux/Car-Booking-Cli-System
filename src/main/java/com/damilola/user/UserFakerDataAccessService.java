f392ebab42ed565fb002cf8095ec7d9b21b3bed8
Michael <dammydeji.dd@gmail.com>
2024-05-12 21:09:20 +0100

    dependencies_and_fake_user_data_added_for_testing_purpose

---
+++ src/main/java/com/damilola/user/UserFakerDataAccessService.java
@@ -0,0 +1,31 @@
+package com.damilola.user;
+
+import com.github.javafaker.Faker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class UserFakerDataAccessService implements UserDao {
+    // For testing purposes
+
+    @Override
+    public User getUserId(UUID id) {
+        return getUsers().stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("❌ User " + id + " does not exist"));
+    }
+
+    @Override
+    public List<User> getUsers() {
+        List<User> userList = new ArrayList<>();
+        Faker faker = new Faker();
+        // Generate 20 random users
+        for (int i = 0; i < 20; i++) {
+            userList.add(new User(UUID.randomUUID(), faker.name().firstName()));
+        }
+        return userList;
+    }
+
+}
