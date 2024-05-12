1a2130f02c16b363650eefc17b0f8e8051bda37d
Michael <dammydeji.dd@gmail.com>
2024-05-12 21:44:35 +0100

    carbookingdao_carbookingservice_added

---
+++ src/main/java/com/damilola/booking/CarBookingDao.java
@@ -0,0 +1,24 @@
+package com.damilola.booking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarBookingDao {
+    private static final List<CarBooking> carBookings;
+
+    static {
+        carBookings = new ArrayList<CarBooking>();
+    }
+
+    public List<CarBooking> getCarBookings() {
+        return carBookings;
+    }
+
+    public void book(CarBooking carBooking) {
+        carBookings.add(carBooking);
+    }
+
+    public void cancel(CarBooking carBooking) {
+        carBookings.remove(carBooking);
+    }
+}
