396105aa581c899a2bd95dc98cc185c23c764cd7
Michael <dammydeji.dd@gmail.com>
2024-05-12 19:51:49 +0100

    Car_service_completed

--- src/main/java/com/damilola/car/CarService.java
+++ src/main/java/com/damilola/car/CarService.java
@@ -1,5 +1,27 @@
 package com.damilola.car;
 
-public class CarService {
+import java.util.List;
 
+public class CarService {
+   private final CarDao carDao;
+
+    public CarService(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
+    public Car getCar(String regNumber){
+     return carDao.getCars().stream()
+             .filter(car -> car.getRegNumber()
+             .equals(regNumber))
+             .findFirst()
+             .orElse(null);
+    }
+
+    public List<Car> getAllCars(){
+        return carDao.getCars();
+    }
+
+    public List<Car> getAllElectricCars(){
+        return getAllCars().stream().filter(Car::isElectric).toList();
+    }
 }
